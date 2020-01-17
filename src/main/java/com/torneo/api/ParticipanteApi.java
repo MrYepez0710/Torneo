e825bf6b4c842635d3cbce5a26620c5a6076337c
Johan Manuel Yepez Vasquez <johan.yepez@datatools.com.co>
2020-01-17 17:11:40 -0500

    DEV: se inicia creación de servicios de participantes

--- src/main/java/com/torneo/api/ParticipanteApi.java
+++ src/main/java/com/torneo/api/ParticipanteApi.java
@@ -1,5 +1,11 @@
 package com.torneo.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class ParticipanteApi {
 
 }
